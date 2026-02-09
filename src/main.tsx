a9052d19f4889ae6cf183e98383473c7faccbfa0
Laxxent <126556352+Laxxent@users.noreply.github.com>
2026-02-09 23:58:54 +0700

    fix: explicit react import for production build

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'; // Tambahkan "React," di sini
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
