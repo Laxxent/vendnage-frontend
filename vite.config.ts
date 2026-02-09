da0d776cb9042545a16d6af351c8133018a0fd12
Laxxent <126556352+Laxxent@users.noreply.github.com>
2026-02-10 00:08:48 +0700

    fix: VITE.CONFIG

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  // TAMBAHKAN BLOK DEFINE INI:
+  define: {
+    'process.env': {},
+    'React': 'React', 
+  },
+  // Blok preview ini boleh tetap ada, tapi tidak berpengaruh ke 'npx serve'
   preview: {
     host: true,
     port: 3000,
